53306d516abb933bf2163e1d1b308bdde5eb50a2
Vegard Langås <sjefen6@gmail.com>
2012-04-19 19:18:38 +0200

    Added timesone setting

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 * @package Example-application
 */
 header('Content-Type: text/html; charset=utf-8');
+date_default_timezone_set("Europe/Berlin");
 require('libs/Smarty.class.php');
 require('menu.class.php');
 require('pageHandler.class.php');
