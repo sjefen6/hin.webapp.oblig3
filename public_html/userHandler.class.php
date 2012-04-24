c7e5960c761e213999c181ef51f9d4d9abfa4a68
Vegard Langås <sjefen6@gmail.com>
2012-04-24 20:27:13 +0200

    Works but genereate 8 random user for each index.php reload

--- public_html/userHandler.class.php
+++ public_html/userHandler.class.php
@@ -68,20 +68,22 @@ class user {
 	private $usermode;
 	private $userlevel;
 
-	function __construct($username, $email, $firstname, $lastname, $password, $userlevel) {
-		// Lets fill thows fields that needs some random stuff
-		$this -> salt = $this -> random_gen(30);
-		$this -> session_cookie = $this -> random_gen(30);
-		$this -> validationkey = $this -> random_gen(30);
-		
-		$this -> username = $username;
-		$this -> email = $email;
-		$this -> firstname = $firstname;
-		$this -> lastname = $lastname;
-		$this -> setPassword($password);
-		$this -> userlevel = $userlevel;
-		
-		$this -> save(true);
+	function __construct($username = null, $email = null, $firstname = null, $lastname = null, $password = null, $userlevel = null) {
+		if ($username == null || $email == null || $firstname == null || $lastname == null || $password == null || $userlevel == null) {
+			// Lets fill thows fields that needs some random stuff
+			$this -> salt = $this -> random_gen(30);
+			$this -> session_cookie = $this -> random_gen(30);
+			$this -> validationkey = $this -> random_gen(30);
+
+			$this -> username = $username;
+			$this -> email = $email;
+			$this -> firstname = $firstname;
+			$this -> lastname = $lastname;
+			$this -> setPassword($password);
+			$this -> userlevel = $userlevel;
+
+			$this -> save(true);
+		}
 	}
 
 	public function getUsername() {
@@ -121,7 +123,7 @@ class user {
 
 	private function random_gen($length) {
 		// Source: http://deepakssn.blogspot.com/2006/06/php-random-string-generator-function.html
-		
+
 		$random = "";
 		srand((double)microtime() * 1000000);
 		$char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -133,49 +135,25 @@ class user {
 		}
 		return $random;
 	}
-	
-	private function save($new = false){
+
+	private function save($new = false) {
 		/*** The SQL SELECT statement ***/
-		if($new) {
-			$sql = "INSERT INTO " . settings::getDbPrefix() . "users " . 
-			"(username, email, firstname, lastname, password, salt, validationkey, session_cookie, usermode, userlevel) " . 
-			"VALUES (:username, :email, :firstname, :lastname, :password, :salt, :validationkey, :session_cookie, :usermode, :userlevel);";
+		if ($new) {
+			$sql = "INSERT INTO " . settings::getDbPrefix() . "users " . "(username, email, firstname, lastname, password, salt, validationkey, session_cookie, usermode, userlevel) " . "VALUES (:username, :email, :firstname, :lastname, :password, :salt, :validationkey, :session_cookie, :usermode, :userlevel);";
 		} else {
-			$sql = "UPDATE " . settings::getDbPrefix() . "users " .
-			"SET username=:username, email=:email, firstname=:firstname, lastname=:lastname, " . 
-			"password=:password, salt=:salt, validationkey=:validationkey, session_cookie=:session_cookie, " . 
-			"usermode=:usermode, userlevel=:userlevel " . 
-        	"WHERE id=:id";
+			$sql = "UPDATE " . settings::getDbPrefix() . "users " . "SET username=:username, email=:email, firstname=:firstname, lastname=:lastname, " . "password=:password, salt=:salt, validationkey=:validationkey, session_cookie=:session_cookie, " . "usermode=:usermode, userlevel=:userlevel " . "WHERE id=:id";
 		}
-		
+
 		/*** fetch into an PDOStatement object ***/
-		$stmt = settings::getDatabase()->prepare($sql);
+		$stmt = settings::getDatabase() -> prepare($sql);
 
 		/*** fetch into the animals class ***/
-		if ($new){
-			$stmt -> execute(array(':username'=>$this -> username,
-								':email'=>$this -> email,
-								':firstname'=>$this -> firstname,
-								':lastname'=>$this -> lastname,
-								':password'=>$this -> password,
-								':salt'=>$this -> salt,
-								':validationkey'=>$this -> validationkey,
-								':session_cookie'=>$this -> session_cookie,
-								':usermode'=>$this -> usermode,
-								':userlevel'=>$this -> userlevel));
+		if ($new) {
+			$stmt -> execute(array(':username' => $this -> username, ':email' => $this -> email, ':firstname' => $this -> firstname, ':lastname' => $this -> lastname, ':password' => $this -> password, ':salt' => $this -> salt, ':validationkey' => $this -> validationkey, ':session_cookie' => $this -> session_cookie, ':usermode' => $this -> usermode, ':userlevel' => $this -> userlevel));
 		} else {
-			$stmt -> execute(array(':id'=>$this -> id,
-								':username'=>$this -> username,
-								':email'=>$this -> email,
-								':firstname'=>$this -> firstname,
-								':lastname'=>$this -> lastname,
-								':password'=>$this -> password,
-								':salt'=>$this -> salt,
-								':validationkey'=>$this -> validationkey,
-								':session_cookie'=>$this -> session_cookie,
-								':usermode'=>$this -> usermode,
-								':userlevel'=>$this -> userlevel));
+			$stmt -> execute(array(':id' => $this -> id, ':username' => $this -> username, ':email' => $this -> email, ':firstname' => $this -> firstname, ':lastname' => $this -> lastname, ':password' => $this -> password, ':salt' => $this -> salt, ':validationkey' => $this -> validationkey, ':session_cookie' => $this -> session_cookie, ':usermode' => $this -> usermode, ':userlevel' => $this -> userlevel));
 		}
 	}
+
 }
 ?>
